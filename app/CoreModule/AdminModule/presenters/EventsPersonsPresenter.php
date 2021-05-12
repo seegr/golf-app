57820b6b822dd12465dcb8d0b75ae365f2423897
Petr Lukáš <seegrcz@gmail.com>
2021-05-12 15:00:41 +0200

    Move person form

--- app/CoreModule/AdminModule/presenters/EventsPersonsPresenter.php
+++ app/CoreModule/AdminModule/presenters/EventsPersonsPresenter.php
@@ -10,188 +10,214 @@ use Monty\Html;
 
 class EventsPersonsPresenter extends AdminPresenter
 {
-  use \App\CoreModule\Traits\EventsTrait;
+    use \App\CoreModule\Traits\EventsTrait;
 
-  /** @var \App\CoreModule\Model\EventsManager @inject */
-  public $EventsManager;
+    /** @var \App\CoreModule\Model\EventsManager @inject */
+    public $EventsManager;
 
-  /** @var \App\CoreModule\FormsModule\Components\FormsFactory @inject */
-  public $FormsFormsFactory;
+    /** @var \App\CoreModule\FormsModule\Components\FormsFactory @inject */
+    public $FormsFormsFactory;
 
-  /** @var \App\CoreModule\FormsModule\Model\FormsManager @inject */
-  public $FormsManager;
+    /** @var \App\CoreModule\FormsModule\Model\FormsManager @inject */
+    public $FormsManager;
 
-  /** @persistentx */
-  public $id;
+    /** @persistentx */
+    public $id;
 
-  /** @persistent */
-  public $date;
+    protected $eventId;
+
+    /** @persistent */
+    public $date;
 
 
-  public function renderEventPersonsList($id, $date): void
-  {
-    $template = $this->template;
-    $list = $this["personsList"];
-    $event = $this->EventsManager->getEvent($id);
-    $template->event = $event;
+    public function renderEventPersonsList($id, $date): void
+    {
+        $template = $this->template;
+        $list = $this["personsList"];
+        $event = $this->EventsManager->getEvent($id);
+        $template->event = $event;
 
-    if ($event->registration == "dates") {
-      if (!$date) {
-        $lastDate = $this->EventsManager->getEventDates($id)->order("start DESC")->fetch();
-        $date = $lastDate->id;
-      }
-      
-      $date = $this->EventsManager->getEventDate($date);
-      bdump($date, "date");
-      $template->date = $date;
+        if ($event->registration == "dates") {
+            if (!$date) {
+                $lastDate = $this->EventsManager->getEventDates($id)->order("start DESC")->fetch();
+                $date = $lastDate->id;
+            }
+
+            $date = $this->EventsManager->getEventDate($date);
+            bdump($date, "date");
+            $template->date = $date;
+        }
+
+        $persons = $this->getEventPersons($id, $date);
+
+        $template->regSummary = $this->EventsManager->getEventRegSummary($id, $date);
+
+        $list->setDataSource($persons);
     }
 
-    $persons = $this->getEventPersons($id, $date);
+    public function actionPersonForm(int $eventId = null, int $id = null, $date = null): void
+    {
+        $template = $this->template;
+        $form = $this["personForm"];
 
-    $template->regSummary = $this->EventsManager->getEventRegSummary($id, $date);
+        if ($eventId) {
+            $event = $this->EventsManager->getEvent($eventId);
+            $template->form = $event->ref("reg_form");
+        } else {
+            $person = $this->EventsManager->getEventPerson($id);
+            $event = $person->ref("event");
+            $personData = $template->person = ArrayHash::from($this->FormsManager->getRecord($id, true));
+            $template->form = $person->ref("record")->ref("form");
+            $form->setDefaults($personData);
+        }
 
-    $list->setDataSource($persons);
-  }
+        $this->eventId = $event->id;
 
-  public function actionPersonForm(int $eventId = null, int $id = null, $date = null): void
-  {
-    $template = $this->template;
-    $form = $this["personForm"];
-
-    if ($eventId) {
-      $event = $this->EventsManager->getEvent($eventId);
-      $template->form = $event->ref("reg_form");
-    } else {
-      $person = $this->EventsManager->getEventPerson($id);
-      $event = $person->ref("event");
-      $personData = $template->person = ArrayHash::from($this->FormsManager->getRecord($id, true));
-      $template->form = $person->ref("record")->ref("form");
-      $form->setDefaults($personData);
+        $template->event = $event;
+        $template->date = $date;
+        $template->formComponent = "personForm";
     }
 
-    $template->event = $event;
-    $template->date = $date;
-    $template->formComponent = "personForm";
-  }
-  
 
-  public function createComponentPersonsList()
-  {
-    $eventId =  $this->getParameter("id");
-    $event = $this->EventsManager->getEvent($eventId);
+    public function createComponentPersonsList()
+    {
+        $eventId = $this->getParameter("id");
+        $event = $this->EventsManager->getEvent($eventId);
 
-    $list = $this->FormsFormsFactory->formRecordsList($event->reg_form);
-    $list->moveColumnStart("lastname");
+        $list = $this->FormsFormsFactory->formRecordsList($event->reg_form);
+        $list->moveColumnStart("lastname");
 
-    $list->setRowCallback(function($i, $tr) {
-      if (!$i["active"]) {
-        $tr->addClass("unactive");
-      }
-    });
+        $list->setRowCallback(function ($i, $tr) {
+            if (!$i["active"]) {
+                $tr->addClass("unactive");
+            }
+        });
 
-    $list->addAction("personForm", "", ":Core:Admin:EventsPersons:personForm", [
-      "id" => "id"
-    ])->setClass("fad fa-pen btn btn-warning");
-    $list->addAction("confirm", "")->setRenderer(function($i) {
-      $person = $this->EventsManager->getEventPerson($i["id"]);
-      $confirmed = $person->ref("state")->short == "confirmed" ? true : false;
-      $a = Html::el("a");
-      $a->class[] = "ajax btn btn-sm fad fa-check";
-      $a->class[] = $confirmed ? "btn-success" : "btn-grey";
-      if ($person->ref("record")->active) {
-        $a->href = $this->link("changePersonState!", [
-          "personId" => $person->record,
-          "state" => $confirmed ? "unconfirmed" : "confirmed"
-        ]);
-      } else {
-        $a->class[] = "disabled";
-      }
-      return $a;
-    });
-    $list->addAction("archive", "")->setRenderer(function($i) {
-      $record = $this->FormsManager->getFormRecord($i["id"]);
-      $a = Html::el("a");
-      $a->class[] = "ajax btn btn-sm fad";
-      $a->class[] = $record->active ? "btn-danger fa-trash" : "btn-primary fa-upload";
-      $a->href = $this->link("personToggle!", [
-        "personId" => $record->id
-      ]);
-      return $a;
-    });
-    // $list->addAction("personToggle", "", "personToggle!", [
-    //   "personId" => "id"
-    // ])->setClass(function($i) {return $i["active"] ? "fad fa-trash btn btn-danger ajax" : "fad fa-trash btn btn-grey ajax";});
+        $list->addAction("personForm", "", ":Core:Admin:EventsPersons:personForm", [
+            "id" => "id"
+        ])->setClass("fad fa-pen btn btn-warning");
+        $list->addAction("confirm", "")->setRenderer(function ($i) {
+            $person = $this->EventsManager->getEventPerson($i["id"]);
+            $confirmed = $person->ref("state")->short == "confirmed" ? true : false;
+            $a = Html::el("a");
+            $a->class[] = "ajax btn btn-sm fad fa-check";
+            $a->class[] = $confirmed ? "btn-success" : "btn-grey";
+            if ($person->ref("record")->active) {
+                $a->href = $this->link("changePersonState!", [
+                    "personId" => $person->record,
+                    "state" => $confirmed ? "unconfirmed" : "confirmed"
+                ]);
+            } else {
+                $a->class[] = "disabled";
+            }
+            return $a;
+        });
+        $list->addAction("archive", "")->setRenderer(function ($i) {
+            $record = $this->FormsManager->getFormRecord($i["id"]);
+            $a = Html::el("a");
+            $a->class[] = "ajax btn btn-sm fad";
+            $a->class[] = $record->active ? "btn-danger fa-trash" : "btn-primary fa-upload";
+            $a->href = $this->link("personToggle!", [
+                "personId" => $record->id
+            ]);
+            return $a;
+        });
+        // $list->addAction("personToggle", "", "personToggle!", [
+        //   "personId" => "id"
+        // ])->setClass(function($i) {return $i["active"] ? "fad fa-trash btn btn-danger ajax" : "fad fa-trash btn btn-grey ajax";});
 
-    // $list->addExportCsv("Export účastníků (CSV)", "ucastnici.csv", "windows-1250")
-    // ->setClass("btn btn-primary");
+        // $list->addExportCsv("Export účastníků (CSV)", "ucastnici.csv", "windows-1250")
+        // ->setClass("btn btn-primary");
 
-    $list->setRefreshUrl(false);
+        $list->setRefreshUrl(false);
 
-    return $list;
-  }
-
-  public function createComponentDateSelectForm()
-  {
-    $f = new Form;
-
-    $id = $this->getParameter("id");
-    $dateFormat = "%d.%m.%Y";
-    $dates = $this->EventsManager->getEventDates($id)->select('*, DATE_FORMAT(start, ?) AS start_date', $dateFormat)->fetchPairs("id", "start_date");
-
-    $f->setMethod("get");
-    $f->addSelect("date", "Termín", $dates)
-    ->setRequired();
-
-    return $f;
-  }
-
-  public function createComponentPersonForm()
-  {
-    $eventId = $this->getParameter("eventId");
-    $id = $this->getParameter("id");
-
-    if ($eventId) {
-      $event = $this->EventsManager->getEvent($eventId);
-    } else {
-      $person = $this->EventsManager->getEventPerson($id);
-      $event = $person->ref("event");
+        return $list;
     }
 
-    $form = $event->ref("reg_form");
+    public function createComponentDateSelectForm()
+    {
+        $f = new Form;
 
-    $f = $this->FormsFormsFactory->customForm($form->id);
+        $id = $this->getParameter("id");
+        $dateFormat = "%d.%m.%Y";
+        $dates = $this->EventsManager->getEventDates($id)->select('*, DATE_FORMAT(start, ?) AS start_date', $dateFormat)->fetchPairs("id", "start_date");
 
-    $f->onSuccess[] = function($f, $v) use ($form, $event) {
-      bdump($v);
-      bdump($this->getParameters(), "pars");
-      $recId = $this->FormsManager->saveRecord($form->id, $v);
+        $f->setMethod("get");
+        $f->addSelect("date", "Termín", $dates)
+            ->setRequired();
 
-      if (empty($v->id)) {
-        $date = $this->getParameter("date");
-        $this->EventsManager->insertEventPerson($event->id, $recId, "part", $date);
-      }
+        return $f;
+    }
 
-      $this->redirect("eventPersonsList", ["id" => $event->id]);
-    };
+    public function createComponentPersonForm()
+    {
+        $eventId = $this->getParameter("eventId");
+        $id = $this->getParameter("id");
 
-    return $f;
-  }
+        if ($eventId) {
+            $event = $this->EventsManager->getEvent($eventId);
+        } else {
+            $person = $this->EventsManager->getEventPerson($id);
+            $event = $person->ref("event");
+        }
 
-  public function handlePersonToggle($personId)
-  {
-    $record = $this->FormsManager->getFormRecord($personId);
-    $record->update(["active" => !$record->active]);
+        $form = $event->ref("reg_form");
 
-    $this->handleChangePersonState($personId, $record->active ? "unconfirmed" : "archived");
-    // $this["personsList"]->reload();
-    $this->redrawControl();
-  }
+        $f = $this->FormsFormsFactory->customForm($form->id);
 
-  public function handleChangePersonState($personId, $state)
-  {
-    $state = $this->EventsManager->getState($state);
-    $this->EventsManager->getEventPerson($personId)->update(["state" => $state->id]);
-    $this["personsList"]->reload();
-  }
+        $f->onSuccess[] = function ($f, $v) use ($form, $event) {
+            bdump($v);
+            bdump($this->getParameters(), "pars");
+            $recId = $this->FormsManager->saveRecord($form->id, $v);
+
+            if (empty($v->id)) {
+                $date = $this->getParameter("date");
+                $this->EventsManager->insertEventPerson($event->id, $recId, "part", $date);
+            }
+
+            $this->redirect("eventPersonsList", ["id" => $event->id]);
+        };
+
+        return $f;
+    }
+
+    public function createComponentMovePersonForm()
+    {
+        $form = new Form;
+
+        $id = $this->getParameter('id');
+
+        $form->addHidden('id', $id);
+        $form->addSelect("event", "Akce")
+            ->setRequired()
+            ->setItems($this->EventsManager->getEvents()->fetchPairs('id', 'title'))
+            ->setDefaultValue($this->eventId);
+        $form->addSubmit('submit', 'Přesunout');
+
+        $form->onSuccess[] = function($f, $v) {
+            $person = $this->EventsManager->getEventPerson($v->id);
+            $person->update(['event' => $v->event]);
+            $this->redirect("this");
+        };
+
+        return $form;
+    }
+
+    public function handlePersonToggle($personId)
+    {
+        $record = $this->FormsManager->getFormRecord($personId);
+        $record->update(["active" => !$record->active]);
+
+        $this->handleChangePersonState($personId, $record->active ? "unconfirmed" : "archived");
+        // $this["personsList"]->reload();
+        $this->redrawControl();
+    }
+
+    public function handleChangePersonState($personId, $state)
+    {
+        $state = $this->EventsManager->getState($state);
+        $this->EventsManager->getEventPerson($personId)->update(["state" => $state->id]);
+        $this["personsList"]->reload();
+    }
 
 }
